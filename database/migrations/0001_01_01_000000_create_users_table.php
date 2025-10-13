c980e8869afcc8faf7b83487237e4518a0c9480b
Poging Programmer <helmorpogi@gmail.com>
2025-10-13 20:50:36 +0800

    refactor: remove password_change

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -21,7 +21,6 @@ return new class () extends Migration {
             $table->string('phone')->nullable();
             $table->text('address')->nullable();
             $table->boolean('is_active')->default(true);
-            $table->boolean('password_changed')->default(false);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
