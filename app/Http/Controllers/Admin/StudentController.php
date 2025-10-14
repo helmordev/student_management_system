aa11e7ff75d1b6509870452a2a4b6077b554ad62
Poging Programmer <helmorpogi@gmail.com>
2025-10-14 21:45:00 +0800

    refactor: remove package

--- app/Http/Controllers/Admin/StudentController.php
+++ app/Http/Controllers/Admin/StudentController.php
@@ -9,6 +9,7 @@ use App\Services\StudentService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 
 class StudentController extends Controller
 {
